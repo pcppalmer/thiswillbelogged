b81241b678666ec175259ba35bc668068290b790
pcppalmer <pcppalmer@gmail.com>
2026-02-08 12:55:38 -0500

    fix use memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, Route, Routes, useParams } from "react-router-dom";
 
 type SubmitResponse =
